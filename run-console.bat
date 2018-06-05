3c0dab2fdd35010c2b3d2f83be70bc6e11d42d28
MaEtUgR <maetugr@gmail.com>
2018-06-05 11:15:13 +0200

    add scripts to add cygwin in PATH and start console

---
+++ run-console.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+
+CALL toolchain\setup-environment-variables.bat x
+
+REM start console
+CALL mintty -
---
+++ toolchain/setup-environment-variables.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+REM catch double clicking
+IF "%1" == "" (
+	ECHO This file is automatically run by the other scripts, no need to execute manually.
+	ECHO Open environment-setup.bat in an editor to modify the toolchain!
+	PAUSE
+)
+
+REM this is the PX4 directory which is one layer above this script
+SET PX4_DIR=%~dp0..
+
+REM home directory for the unix environment
+SET HOME=%PX4_DIR%\home
+
+REM path to Cygwin
+SET PATH=%PX4_DIR%\toolchain\cygwin64\bin
