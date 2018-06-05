0f70ab38f281e4a6511c2d5e592dc88b3970caaa
MaEtUgR <maetugr@gmail.com>
2018-06-05 16:36:16 +0200

    add missing python package empy

--- toolchain/cygwin64/install-cygwin-python-packages.bat
+++ toolchain/cygwin64/install-cygwin-python-packages.bat
@@ -3,4 +3,4 @@
 CALL ..\setup-environment-variables.bat x
 
 REM install needed paython packages under cygwin
-CALL bash -c "pip2 install toml && pip2 install pyserial && pip2 install pyulog"
+CALL bash -c "pip2 install toml && pip2 install pyserial && pip2 install pyulog && pip2 install empy"
