91f9067148f9e5a13abc41482b9b84a729bac234
MaEtUgR <maetugr@gmail.com>
2018-06-08 13:41:24 +0200

    add python package install to all components

--- toolchain/install-all-components.bat
+++ toolchain/install-all-components.bat
@@ -5,6 +5,7 @@ REM -- Change to the directory of the executing batch file
 CD %~dp0
 
 CALL cygwin64\install-cygwin-px4.bat
+CALL cygwin64\install-cygwin-python-packages.bat
 CALL gcc-arm\install-gcc-arm.bat
 CALL jdk\install-jdk.bat
 CALL apache-ant\install-apache-ant.bat
