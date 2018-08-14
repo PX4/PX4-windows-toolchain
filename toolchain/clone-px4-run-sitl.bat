6f11f1e82c5e709cbb54a2cd00b605b2b5acc046
Matthias Grob <maetugr@gmail.com>
2018-08-14 19:05:20 +0200

    add clone-px4-run-sitl for convenience after installation
    If the user checks the box in the end of the installation this script
    is executed.

---
+++ toolchain/clone-px4-run-sitl.bat
@@ -0,0 +1,14 @@
+@echo off
+
+pushd %~dp0
+call setup-environment-variables.bat x
+popd
+
+pushd %PX4_DIR%
+
+if not exist ".\home\" mkdir home
+cd home
+
+:: start clone PX4 repo, build and run SITL (login shell required because python modules need /usr/local/bin in the PATH!)
+call mintty -e bash --login -c "trap bash SIGINT; git clone --recursive -j8 https://github.com/PX4/Firmware.git ; cd Firmware && make posix jmavsim"
+popd
