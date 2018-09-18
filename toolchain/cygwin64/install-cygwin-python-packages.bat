6f752940919211953008cb2b08ba1243fb176a0c
Matthias Grob <maetugr@gmail.com>
2018-09-18 15:34:42 +0200

    install-cygwin-python: add pyyaml
    because of new requirements by the RTPS message generation.

--- toolchain/cygwin64/install-cygwin-python-packages.bat
+++ toolchain/cygwin64/install-cygwin-python-packages.bat
@@ -5,6 +5,6 @@ CD %~dp0
 CALL ..\setup-environment-variables.bat x
 
 REM install needed paython packages under cygwin
-CALL bash -c "pip2 install toml && pip2 install pyserial && pip2 install pyulog && pip2 install empy"
+CALL bash -c "pip2 install toml && pip2 install pyserial && pip2 install pyulog && pip2 install empy && pip2 install pyyaml"
 
 ENDLOCAL
