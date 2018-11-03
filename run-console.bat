18af5746f73ece60eddc172acbceeae916145cac
Matthias Grob <maetugr@gmail.com>
2018-11-03 14:18:52 +0100

    run-console: restore path in bash

--- run-console.bat
+++ run-console.bat
@@ -3,5 +3,13 @@ PUSHD %~dp0
 CALL toolchain\setup-environment-variables.bat x
 POPD
 
-REM start logged in bash terminal
-CALL bash -l
+SET PREVIOUS_PATH=%CD%
+
+REM start bash terminal
+REM login shell required because python modules need /usr/local/bin in the PATH!
+REM if we run the batch script by double click stay in home folder else restore path
+IF EXIST %~nx0 (
+	CALL bash -l
+) ELSE (
+	CALL bash -l -c "cd $PREVIOUS_PATH && bash"
+)
