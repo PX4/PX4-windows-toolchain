1c7665838b6e5a3935193f92d830424606373f0a
Matthias Grob <maetugr@gmail.com>
2018-11-04 18:52:28 +0000

    setup-environment: detect windows git installation
    for gitk and git-gui availability inside cygwin

--- toolchain/setup-environment-variables.bat
+++ toolchain/setup-environment-variables.bat
@@ -10,6 +10,9 @@ IF "%1" == "" (
 REM this is the PX4 directory which is one layer above this script
 SET PX4_DIR=%~dp0..
 
+REM backup windows path variable to detect windows git installation
+SET WINDOWS_PATH=%PATH%
+
 REM path to Cygwin Unix Environment
 SET PATH=%PX4_DIR%\toolchain\cygwin64\bin
 REM path to GCC for ARM Compiler
@@ -20,11 +23,15 @@ REM path to Apache Ant, a Java build tool
 SET PATH=%PATH%;%PX4_DIR%\toolchain\apache-ant\bin
 REM path to genromfs
 SET PATH=%PATH%;%PX4_DIR%\toolchain\genromfs\
+REM path to shell scripts
+SET PATH=%PATH%;%PX4_DIR%\toolchain\scripts\
 
-REM home directory for the unix environment
-SET HOME=%PX4_DIR%\home
-REM also set it for cygwin in general to make ssh happy
-CALL bash -c "sed -i '/db_home:/c\db_home:  '$HOME /etc/nsswitch.conf"
+REM determine home directory
+cygpath -u %PX4_DIR%\home > tmp_cygpath
+SET /p PX4_HOME= <tmp_cygpath
+del tmp_cygpath
+REM set home directory for the unix environment
+CALL bash -c "sed -i '/db_home:/c\db_home:  '$PX4_HOME /etc/nsswitch.conf"
 
 REM create optional symlinks to system wide .gitconfig and .ssh
 CALL bash %PX4_DIR%\toolchain\configuration-symlinks.sh
