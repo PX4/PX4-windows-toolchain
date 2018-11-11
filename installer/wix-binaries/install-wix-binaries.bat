cd1cada8a7b8b5cf5214ab890c09c893fd3169d3
Matthias Grob <maetugr@gmail.com>
2018-11-11 09:48:02 +0100

    wix-binaries: add installer script

---
+++ installer/wix-binaries/install-wix-binaries.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+SETLOCAL
+ECHO ** Installing WiX Toolset for PX4 Toolchain installer generation
+ECHO Note: This script requires the Cygwin environment to be installed already
+CD %~dp0
+CALL ..\..\toolchain\setup-environment-variables.bat x
+
+ECHO *** Downloading WiX Toolset
+REM install gcc-arm in this folder
+CALL bash -c "wget -O wix-binaries.zip -nc https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311-binaries.zip && unzip wix-binaries.zip && rm wix-binaries.zip"
+
+ENDLOCAL
