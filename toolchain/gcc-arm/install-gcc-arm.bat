3e559e39b41baee5c0f455e2cf1c3dfcd6b94c84
Matthias Grob <maetugr@gmail.com>
2018-11-16 17:11:31 +0100

    install scripts: use unzip in quiet verbosity
    To not list all inflated files.

--- toolchain/gcc-arm/install-gcc-arm.bat
+++ toolchain/gcc-arm/install-gcc-arm.bat
@@ -5,6 +5,6 @@ CD %~dp0
 CALL ..\setup-environment-variables.bat x
 
 REM install gcc-arm in this folder
-CALL bash -c "wget -O gcc-arm.zip -nc https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-7-2017-q4-major-win32.zip && unzip gcc-arm.zip && rm gcc-arm.zip"
+CALL bash -c "wget -O gcc-arm.zip -nc https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-7-2017-q4-major-win32.zip && unzip -q gcc-arm.zip && rm gcc-arm.zip"
 
 ENDLOCAL
