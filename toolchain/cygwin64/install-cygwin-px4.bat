37aa465eabb755a0ee5a30d8fa62070bf92efcf2
Matthias Grob <maetugr@gmail.com>
2018-11-18 17:34:43 +0000

    install-cygwin: remove dependency on setup-environment
    Setup-environment assumes meanwhile that cygwin is already installed
    using it to determine the installation directory for cygwin therefore
    resulted in errors. In this commit proper bootstrapping is reestablished.

--- toolchain/cygwin64/install-cygwin-px4.bat
+++ toolchain/cygwin64/install-cygwin-px4.bat
@@ -6,12 +6,10 @@ CD %~dp0
 ECHO *** Downloading Cygwin Setup
 certutil.exe -urlcache -split -f "https://cygwin.com/setup-x86_64.exe" setup-x86_64.exe
 
-CALL ..\setup-environment-variables.bat x
-
 REM -- Configure our paths
 SET SITE=http://mirrors.kernel.org/sourceware/cygwin/
 SET LOCALDIR=%TEMP%/cygwin-installation-files
-SET ROOTDIR=%PX4_DIR%/toolchain/cygwin64
+SET ROOTDIR=%CD%
 
 REM -- These are the packages we will install (in addition to the default packages)
 SET PACKAGES=cmake,gcc-g++,git,make,ninja,patch,xxd,nano,python2,python2-pip,python2-numpy,python2-jinja2,unzip,astyle,bash-completion,wget,libcurl-devel,procps-ng
