4a7f392228710f6fd6928dfc50aa4acb27eecb72
Matthias Grob <maetugr@gmail.com>
2018-11-18 17:38:58 +0000

    install-cygwin: refactor comments

--- toolchain/cygwin64/install-cygwin-px4.bat
+++ toolchain/cygwin64/install-cygwin-px4.bat
@@ -6,15 +6,15 @@ CD %~dp0
 ECHO *** Downloading Cygwin Setup
 certutil.exe -urlcache -split -f "https://cygwin.com/setup-x86_64.exe" setup-x86_64.exe
 
-REM -- Configure our paths
+REM configure download url, temporary and installation directories
 SET SITE=http://mirrors.kernel.org/sourceware/cygwin/
 SET LOCALDIR=%TEMP%/cygwin-installation-files
 SET ROOTDIR=%CD%
 
-REM -- These are the packages we will install (in addition to the default packages)
+REM configure packages we will install (in addition to the default packages)
 SET PACKAGES=cmake,gcc-g++,git,make,ninja,patch,xxd,nano,python2,python2-pip,python2-numpy,python2-jinja2,unzip,astyle,bash-completion,wget,libcurl-devel,procps-ng
 
-REM -- Do it!
+REM do installation
 ECHO *** Installing Packages
 ECHO Package list: %PACKAGES%
 setup-x86_64.exe -q -D -L -n -g -o -W -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -C Base -P %PACKAGES% > cygwin_setup_ouput.log
