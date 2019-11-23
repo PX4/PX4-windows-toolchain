c8a3fed5fcf07401dd4b0c52334aa6334ace7431
Matthias Grob <maetugr@gmail.com>
2019-11-23 12:03:44 +0100

    install-apache-ant: switch download URL to working mirror
    Found on https://ant.apache.org/bindownload.cgi

--- toolchain/apache-ant/install-apache-ant.bat
+++ toolchain/apache-ant/install-apache-ant.bat
@@ -5,5 +5,5 @@ CD %~dp0
 CALL ..\scripts\setup-environment.bat x
 
 REM install apache-ant in this folder
-CALL bash -c "wget -nv -O apache-ant.zip -nc http://www.pirbot.com/mirrors/apache//ant/binaries/apache-ant-1.10.7-bin.zip && unzip -q apache-ant.zip && rm apache-ant.zip && f=(./*) && mv ./*/* . && rmdir "${f}""
+CALL bash -c "wget -nv -O apache-ant.zip -nc https://www-us.apache.org/dist//ant/binaries/apache-ant-1.10.7-bin.zip && unzip -q apache-ant.zip && rm apache-ant.zip && f=(./*) && mv ./*/* . && rmdir "${f}""
 ENDLOCAL
