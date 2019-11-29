8f8fbb42052a3718429534a7ee8f2965277dee52
Matthias Grob <maetugr@gmail.com>
2019-11-29 13:45:32 +0100

    install-cygwin-python: add pyros-genmsg package
    like it was requested in https://github.com/PX4/Firmware/pull/13572
    to be able to remove gencpp and genmsg submodules.

--- toolchain/cygwin64/install-cygwin-python-packages.bat
+++ toolchain/cygwin64/install-cygwin-python-packages.bat
@@ -5,8 +5,8 @@ CD %~dp0
 CALL ..\scripts\setup-environment.bat x
 
 REM install needed Python 2 packages under cygwin
-CALL bash -c "pip2 -q --disable-pip-version-check install toml pyserial pyulog empy pyyaml cerberus"
+CALL bash -c "pip2 -q --disable-pip-version-check install toml pyserial pyulog empy pyyaml cerberus pyros-genmsg"
 REM install needed Python 3 packages under cygwin
-CALL bash -c "pip3 -q --disable-pip-version-check install toml pyserial pyulog empy pyyaml cerberus"
+CALL bash -c "pip3 -q --disable-pip-version-check install toml pyserial pyulog empy pyyaml cerberus pyros-genmsg"
 
 ENDLOCAL
