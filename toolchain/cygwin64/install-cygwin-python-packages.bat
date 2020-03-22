f08348b85fd502fea99b3ee6dfd5c7da0e8ecce1
Matthias Grob <maetugr@gmail.com>
2020-03-22 12:48:13 +0100

    install-cygwin-python-packages: add default python symbolic link

--- toolchain/cygwin64/install-cygwin-python-packages.bat
+++ toolchain/cygwin64/install-cygwin-python-packages.bat
@@ -4,6 +4,8 @@ ECHO ** Installing Python Packages in Cygwin for PX4 Toolchain
 CD %~dp0
 CALL ..\scripts\setup-environment.bat x
 
+REM create symbolic link to make python3 the default python binary
+CALL bash -c "ln -sf /usr/bin/python3 /usr/local/bin/python"
 REM install needed Python 2 packages under cygwin
 CALL bash -c "pip2 -q --disable-pip-version-check install toml pyserial pyulog empy pyyaml cerberus pyros-genmsg"
 REM install needed Python 3 packages under cygwin
