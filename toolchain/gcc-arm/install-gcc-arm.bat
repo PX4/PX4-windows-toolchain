b584666aa2ec010c9eba12e4c1966846c3c9cece
Matthias Grob <maetugr@gmail.com>
2020-10-26 17:19:55 +0100

    install-gcc-arm: update to latest GCC 9

--- toolchain/gcc-arm/install-gcc-arm.bat
+++ toolchain/gcc-arm/install-gcc-arm.bat
@@ -5,6 +5,6 @@ CD %~dp0
 CALL ..\scripts\setup-environment.bat x
 
 REM install gcc-arm in this folder
-CALL bash -c "wget -nv -O gcc-arm.zip -nc https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-win32.zip && unzip -q gcc-arm.zip && rm gcc-arm.zip"
+CALL bash -c "wget -nv -O gcc-arm.zip -nc https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-win32.zip && unzip -q gcc-arm.zip && rm gcc-arm.zip"
 
 ENDLOCAL
