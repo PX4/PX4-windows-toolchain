198e94b6a97aee94b3f1b9229fea0be44ff1f20a
Matthias Grob <maetugr@gmail.com>
2020-10-26 17:21:11 +0100

    install-jdk: switch to a new version of openJDK

--- toolchain/jdk/install-jdk.bat
+++ toolchain/jdk/install-jdk.bat
@@ -5,6 +5,6 @@ CD %~dp0
 CALL ..\scripts\setup-environment.bat x
 
 REM install jdk
-CALL bash -c "wget -nv -O jdk.zip -nc https://s3-us-west-2.amazonaws.com/px4-tools/PX4+Windows+Cygwin+Toolchain/jdk1.8.0_152.zip && unzip -q jdk.zip && rm jdk.zip"
+CALL bash -c "wget -nv -O jdk.zip -nc https://download.java.net/java/GA/jdk15.0.1/51f4f36ad4ef43e39d0dfdbaf6549e32/9/GPL/openjdk-15.0.1_windows-x64_bin.zip && unzip -q jdk.zip && rm jdk.zip && f=(./*) && mv ./*/* . && rmdir "${f}""
 
 ENDLOCAL
