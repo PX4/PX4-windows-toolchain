641b61e68ac6e47867b0e8c6521f5471e60644bd
Matthias Grob <maetugr@gmail.com>
2020-10-26 17:36:23 +0100

    clone-px4-build/run-sitl: correct make target for SITL

--- toolchain/scripts/clone-px4-run-sitl.bat
+++ toolchain/scripts/clone-px4-run-sitl.bat
@@ -10,5 +10,5 @@ if not exist ".\home\" mkdir home
 cd home
 
 :: start clone PX4 repo, build and run SITL (login shell required because python modules need /usr/local/bin in the PATH!)
-call mintty -e bash --login -c "trap bash SIGINT; git clone --recursive -j8 https://github.com/PX4/Firmware.git ; cd Firmware && make posix jmavsim"
+call mintty -e bash --login -c "trap bash SIGINT; git clone --recursive -j8 https://github.com/PX4/Firmware.git ; cd Firmware && make px4_sitl jmavsim"
 popd
